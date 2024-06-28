05b2846ba332488f5e176bf2ebb9c7634b3d6b5a
ItsWachira <brianwachira79@gmail.com>
2024-06-28 14:30:01 +0300

    feat: add user authentication from local

--- src/main/java/com/example/permify/auth/api/config/SecurityConfig.java
+++ src/main/java/com/example/permify/auth/api/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .formLogin().disable() // Disable the default login form
+            .formLogin().disable() /
             .authorizeRequests()
             .requestMatchers("/api/auth/login").permitAll()
             .requestMatchers("/api/home").hasAuthority("ADMIN") // Restrict access to /rest/home to ADMIN role
