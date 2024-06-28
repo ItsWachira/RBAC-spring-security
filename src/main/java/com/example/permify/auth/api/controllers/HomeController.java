9fa3faa65fef739e5b138b40ae2428e5d26cb8d5
ItsWachira <brianwachira79@gmail.com>
2024-06-28 13:12:01 +0300

    Automated commit

--- src/main/java/com/example/permify/auth/api/controllers/HomeController.java
+++ src/main/java/com/example/permify/auth/api/controllers/HomeController.java
@@ -1,14 +1,12 @@
 package com.example.permify.auth.api.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Controller
 @RequestMapping("/api/home")
