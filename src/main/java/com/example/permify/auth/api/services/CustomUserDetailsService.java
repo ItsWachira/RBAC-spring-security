aca9bb85a25e65882138cef3a307c84a554ddf92
ItsWachira <brianwachira79@gmail.com>
2024-07-04 16:04:01 +0300

    chore: remove deprecated files from local directory

--- src/main/java/com/example/permify/auth/api/services/CustomUserDetailsService.java
+++ src/main/java/com/example/permify/auth/api/services/CustomUserDetailsService.java
@@ -18,22 +18,17 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.userRepository = userRepository;
     }
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findUserByEmail(email);
-        List<String> roles = new ArrayList<>();
+public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    User user = userRepository.findUserByEmail(email);
+    Role role = user.getRole();
+    String roleName = role != null ? "ROLE_" + role.getName() : null;
 
-        if (email.equals("admin@example.com")) {
-            roles.add("ADMIN"); 
-        } else {
-            roles.add("USER"); 
-        }
-
-        UserDetails userDetails =
-                org.springframework.security.core.userdetails.User.builder()
-                        .username(user.getEmail())
-                        .password(user.getPassword())
-                        .roles(roles.toArray(new String[0]))
-                        .build();
-        return userDetails;
-    }
+    UserDetails userDetails =
+            org.springframework.security.core.userdetails.User.builder()
+                    .username(user.getEmail())
+                    .password(user.getPassword())
+                    .roles(roleName != null ? new String[]{roleName} : new String[0])
+                    .build();
+    return userDetails;
+}
 }
